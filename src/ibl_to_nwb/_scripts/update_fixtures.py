f46373ede87e12bb5d4d261476795f1da51d09df
Georg Raiser <grg2rsr@gmail.com>
2025-06-26 12:03:18 +0100

    updating fixtures units and recordings table

--- src/ibl_to_nwb/_scripts/update_fixtures.py
+++ src/ibl_to_nwb/_scripts/update_fixtures.py
@@ -3,16 +3,19 @@
 # download bwm units table 
 from pathlib import Path
 
-from brainwidemap import bwm_query, bwm_units
+from brainwidemap import bwm_query, bwm_units, download_aggregate_tables
 from one.api import ONE
+import pandas as pd
 
 one = ONE()
 
 # saving bwm_units_df.pqt
-bwm_units_df = bwm_units(one, min_qc=0)
+# bwm_units_df = bwm_units(one, min_qc=0)
+bwm_units_df = pd.read_parquet(download_aggregate_tables(one=one))
 outpath = Path(__file__).parent.parent / 'fixtures' / 'bwm_units_df.pqt'
 outpath.parent.mkdir(parents=True, exist_ok=True)
 bwm_units_df.to_parquet(outpath)
+print(bwm_units_df.shape[0])
 
 # saving bwm_df
 # TODO proper freeze, also return_details and freeze together doesn't work
